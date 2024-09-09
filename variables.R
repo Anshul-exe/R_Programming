d6ceda9ebe47f8bbd0af2216ea193efd5d621473
Anshul-exe <anshulchauhanworks@gmail.com>
2024-09-09 13:15:30 +0530

    class shit

---
+++ variables.R
@@ -0,0 +1,21 @@
+num <- 5
+typeof(num)
+
+str <- "Hello"
+typeof(str)
+
+num2 <- 5.88
+typeof(num2)
+
+vec <- c(2L, 3L, 4L, 5L)
+typeof(vec)
+# class(vec)
+# length(vec)
+
+com <- 3 + 4i
+typeof(com)
+
+lg <- log(x = 5, base = 10)
+
+print(lg)
+typeof(lg)
