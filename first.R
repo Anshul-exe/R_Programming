03394f3dd286e3c6dfcaa88e01aaab3f20cfccff
Anshul-exe <anshulchauhanworks@gmail.com>
2024-09-17 01:10:25 +0530

    changes in plot

--- first.R
+++ first.R
@@ -2,11 +2,17 @@
 cars <- c(1, 3, 6, 4, 9)
 trucks <- c(2, 5, 4, 5, 12)
 
+# Open a PNG file for the plot
+png("plot.png")
+
 # Graph cars using a y-axis that ranges from 0 to 12
-plot(cars, type="o", col="blue", ylim=c(0,12))
+plot(cars, type = "o", col = "blue", ylim = c(0, 12))
 
 # Graph trucks with red dashed lines and square points
-lines(trucks, type="o", pch=22, lty=2, col="red")
+lines(trucks, type = "o", pch = 22, lty = 2, col = "red")
 
 # Create a title with a red, bold/italic font
-title(main="Autos", col.main="red", font.main=4)
+title(main = "Autos", col.main = "red", font.main = 4)
+
+# Close the file device
+dev.off()
