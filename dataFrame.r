3f9dce4d8aa2486562c1b4cfd22009a54644eeb4
Anshul Singh Chauhan <anshulchauhanworks@gmail.com>
2024-10-26 20:35:59 +0530

    data frame

---
+++ dataFrame.r
@@ -0,0 +1,10 @@
+# Creating a data frame
+df <- data.frame(
+  Name = c("Alice", "Bob", "Charlie"),
+  Age = c(25, 30, 35),
+  Score = c(90.5, 85.0, 88.0),
+  Gender = factor(c("Female", "Male", "Male"))
+)
+
+# Viewing the data frame
+print(df)
