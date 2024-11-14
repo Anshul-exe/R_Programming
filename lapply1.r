48ae1411adaec1ae3e6289a4a0b1c694c85061d3
Anshul Singh Chauhan <anshulchauhanworks@gmail.com>
2024-11-14 13:09:22 +0530

    test ke phle

--- lapply1.r
+++ lapply1.r
@@ -1,9 +1,2 @@
-function (X, FUN, ...)
-{
-  FUN <- match.fun(FUN)
-  if (!is.vector(X) || is.object(X))
-  X <- as.list(X)
-  .Internal(lapply(X, FUN))
-}
-<bytecode: 0x7ff7a1951c00>
-  <environment: namespace:base>
+x <- list(a = 1:4, b = rnorm(10), c = rnorm(20, 1), d = rnorm(100, 5))
+lapply(x, mean)
