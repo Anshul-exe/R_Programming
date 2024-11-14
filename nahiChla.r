19946175a78fb3bfe2f65377737a456ed7e1086d
Anshul Singh Chauhan <anshulchauhanworks@gmail.com>
2024-11-15 04:00:56 +0530

    hata diya lendi ka

--- nahiChla.r
+++
@@ -1,9 +0,0 @@
-function (X, FUN, ...)
-{
-  FUN <- match.fun(FUN)
-  if (!is.vector(X) || is.object(X))
-  X <- as.list(X)
-  .Internal(lapply(X, FUN))
-}
-<bytecode: 0x7ff7a1951c00>
-  <environment: namespace:base>
